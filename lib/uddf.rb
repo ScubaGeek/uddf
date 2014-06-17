a85b08ef57a52c01302fa1d87ffbe2baf2b04881
Donovan C. Young <dyoung522@gmail.com>
2014-06-16 23:43:47 -0400

    Refactor tests to include UDDF::Base testing

--- lib/uddf.rb
+++ lib/uddf.rb
@@ -9,14 +9,20 @@ module UDDF
 
   # For inheritance
   class Base
+    def initialize(args = {})
+      args.each do |key,value|
+        instance_variable_set("@#{key}", value)
+      end
+    end
+
     def nodename
       self.class.to_s.split('::')[-1].downcase
     end
 
     def nodes
       node_arr = Hash.new
-      self.instance_variables.each do |var|
-          node_arr[var.to_s.gsub(/^@/, '').to_sym] = self.instance_variable_get(var)
+      instance_variables.each do |var|
+        node_arr[var.to_s.gsub(/^@/, '').to_sym] = instance_variable_get(var)
       end
       return node_arr
     end
@@ -24,6 +30,30 @@ module UDDF
     def to_elem
       XML.to_elem(self)
     end
+
+    def method_missing(method, *args, &block)
+      return super unless nodes.is_a?(Hash)
+
+      /^(?<meth>\w*)(?<oper>\W*)$/ =~ method
+      meth_sym = "@#{meth}".to_sym
+
+      if nodes.has_key?(meth.to_sym)
+        case oper
+          when ''  then instance_variable_get(meth_sym)
+          when '=' then instance_variable_set(meth_sym, *args)
+          else super
+        end
+      else
+        super
+      end
+    end
+
+    def respond_to?(method, include_private = false)
+      return super unless nodes.is_a?(Hash)
+      return true if nodes.has_key?(method.to_s.gsub(/\W*$/, '').to_sym)
+      super
+    end
+
   end
 end
 
--- test/tests/test_uddf.rb
+++ test/tests/test_uddf.rb
@@ -11,5 +11,45 @@ class TestUDDF < Minitest::Test
     assert_respond_to doc, :to_uddf, %q{UDDF::Document doesn't respond to 'to_uddf'}
     assert_instance_of String, UDDF.new.to_xml, %q{UDDF#to_xml doesn't produce valid output}
   end
+
+end
+
+class TestBase < Minitest::Test
+  def setup
+    @t = UDDF::Base.new(foo: 'bar')
+  end
+
+  def test_nodes
+    assert_respond_to @t, :nodes
+    assert_instance_of Hash, @t.nodes
+    assert_equal @t.nodes, { :foo => 'bar' }
+  end
+
+  def test_nodename
+    assert_respond_to @t, :nodename
+    assert_equal 'base', @t.nodename
+  end
+
+  def test_instance_variables
+    assert_equal [:@foo], @t.instance_variables
+    assert_equal 'bar', @t.instance_variable_get(:@foo)
+  end
+
+  def test_instance_variable_getter
+    assert_respond_to @t, :foo
+    assert_equal 'bar', @t.foo
+  end
+
+  def test_instance_variable_setter
+    assert_respond_to @t, :foo=
+    @t.foo = 'Baz'
+    assert_equal 'Baz', @t.foo
+  end
+
+  def test_invalid_method
+    assert_raises NoMethodError do
+      @t.baz
+    end
+  end
 end
 
